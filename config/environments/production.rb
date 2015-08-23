b660b7880373c0f3797c0a0da44969fa354c42cf
Anna <annagaiel@gmail.com>
2015-08-23 15:21:08 +0000

    Added Devise user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Grasper::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'grasper.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
